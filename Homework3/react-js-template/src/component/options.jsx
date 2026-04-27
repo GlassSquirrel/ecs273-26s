3293beb316ab1227cced7f67930f1da4db0931f6
GlassSquirrel <jintianx9953@gmail.com>
2026-04-27 15:12:22 -0700

    add data from last homeworks, manually add tickers in options.jsx for dropdown menu

--- Homework3/react-js-template/src/component/options.jsx
+++ Homework3/react-js-template/src/component/options.jsx
@@ -1,10 +1,19 @@
-import Data from "../../data/demo.json";
+// import Data from "../../data/demo.json";
+
+const stockTickers = [
+  'XOM', 'CVX', 'HAL',
+  'MMM', 'CAT', 'DAL',
+  'MCD', 'NKE', 'KO',
+  'JNJ', 'PFE', 'UNH',
+  'JPM', 'GS', 'BAC',
+  'AAPL', 'MSFT', 'NVDA', 'GOOGL', 'META'
+]
 
 export default function RenderOptions() {
-    const bars = Data.data;
-    return bars.map((bar, index) => (
-      <option key={index} value={bar.category}>
-        {bar.category}
+    // const bars = Data.data;
+    return stockTickers.map((ticker, index) => (
+      <option key={index} value={ticker}>
+        {ticker}
       </option>
     ));
   }
